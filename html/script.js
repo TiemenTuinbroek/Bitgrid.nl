d55e4f3631832acb3f7811cb42cad5dbb43f7643
Tiemen Tuinbroek <50925639+TiemenTuinbroek@users.noreply.github.com>
2021-03-12 16:18:49 +0100

    discord, welkom lttle change anim not working

--- html/script.js
+++ html/script.js
@@ -1,3 +1,21 @@
-$(window).scroll(function(){
-	$('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
-}); 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("change").className = "wit";
+  } else {
+    document.getElementById("change").className = "";
+  }
+}
+
+function myFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("kaart").className = "slideUp";
+  }
+}
+
+function myFunction() {
+	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+	  document.getElementById("begin").className = "slideUp";
+	}
+  }
