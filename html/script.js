5fab1e8950eb61117f36963edbb2d09d52e0096f
Tiemen Tuinbroek <50925639+TiemenTuinbroek@users.noreply.github.com>
2021-03-16 17:23:58 +0100

    model carrousel

--- html/script.js
+++ html/script.js
@@ -1,12 +1,9 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
 window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    document.getElementById("change").className = "wit";
-  } else {
-    document.getElementById("change").className = "";
-  }
-}
 
 function myFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
