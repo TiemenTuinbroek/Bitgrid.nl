ff2e53adb5eda1058c74f69c3487eeaff1f87c6b
Tiemen Tuinbroek <50925639+TiemenTuinbroek@users.noreply.github.com>
2021-03-23 15:10:48 +0100

    js done

--- html/script.js
+++ html/script.js
@@ -1,15 +1,7 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-}
-
 window.onscroll = function() {myFunction()};
 
 
-function myFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    document.getElementById("kaart").className = "slideUp";
-  }
-}
+
 
 function myFunction() {
 	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
